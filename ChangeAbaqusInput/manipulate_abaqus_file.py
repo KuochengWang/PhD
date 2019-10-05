2159ea71ba8642ce883a89457355578151e45d34
KuochengWang <30810210+KuochengWang@users.noreply.github.com>
2019-10-05 17:31:14 -0500

    add read_part function

--- ChangeAbaqusInput/manipulate_abaqus_file.py
+++ ChangeAbaqusInput/manipulate_abaqus_file.py
@@ -9,21 +9,14 @@ import pdb
 class ReadAbaqusInput:
     def __init__(self, file_name):
         self.file_name = file_name
-    
-    def insert(self, section_name, content, output_file):
-        f = open(self.output_file, "w")
-        contents = f.readlines()
-        index = self.find_line_index(section_name)
-        contents.insert(index, content)
-        contents = "".join(contents)
-        f.write(contents)
-        f.close()
         
     # take a list of nodes position, add to .inp file
     # @param nodes a list of numpy with 1x3, or a list of list
+    # @param part the name of the part
     # @param output_file the .inp need to be written into 
-    def add_node(self, nodes, output_file):
-        contents, index = self.return_content_and_insert_position('*Node\n')
+    def add_node(self, nodes, output_file, part):
+        contents, index = self.return_content_and_insert_position(part)
+        index += 2
         for i, node in enumerate(nodes):
             pos = str(i + 1) + ',' + str(node[0]) + ',' + str(node[1]) + ',' + str(node[2]) + '\n'
             contents.insert(index + i, pos)
@@ -31,19 +24,28 @@ class ReadAbaqusInput:
         f = open(output_file, "w")
         f.write(contents)
         f.close()
-            
+    
+    # find index of the string of the line need to be found
     def return_content_and_insert_position(self, search_line):
         output_file = open(self.file_name, 'r')
         content = output_file.readlines()
         output_file.close()
         line_index = content.index(search_line)
-        return content, line_index + 1
-
-    def find_line_index(self, section_name):
-        index = 0
-        with open(self.file_name, "r") as file:
-             for line in file:
-                 if line.strip() == section_name:
-                     return index
-                 index += 1
-        return index + 1
+        return content, line_index
+    
+    # read the coordinate from the contents
+    # @param start_index the starting line to read the file
+    def __read_coordinates__(self, contents, start_index):
+        coords = []
+        for index, coord in enumerate(contents[start_index:-1]):
+            coord = coord.strip().split(',')
+            if not coord[0].isdigit():
+                break
+            coords.append([float(coord[1]), float(coord[2]), float(coord[3])])
+        return coords
+    
+    # find all content 
+    def read_part(self, part):
+        contents, index = self.return_content_and_insert_position(part)
+        return self.__read_coordinates__(contents, index + 2)
+        
