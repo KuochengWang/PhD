57c2db263c7730bbe60cbdb080a287952d6363ac
KuochengWang <30810210+KuochengWang@users.noreply.github.com>
2019-10-13 15:42:56 -0500

    added writing to elset

--- ChangeAbaqusInput/manipulate_abaqus_file.py
+++ ChangeAbaqusInput/manipulate_abaqus_file.py
@@ -65,7 +65,7 @@ class ReadAbaqusInput:
         self.contents[index + 2] = 'Set-6, 2, 2, ' + str(direction[1]) + '\n'
         self.contents[index + 3] = 'Set-6, 3, 3, ' + str(direction[2]) + '\n'
         
-        
+    
     # return the translation vector, x, y, z, and the index of the line
     def read_assembly(self, start_line):
          index = self.return_insert_position(start_line)
@@ -86,4 +86,47 @@ class ReadAbaqusInput:
         index = self.return_insert_position(start_line)
         return float(self.contents[index + 1].strip().split(',')[1])
     
-    
+    # set element set in .inp
+    # arguments:
+    # start_line: the line that starts the element set
+    # element: a list of numbers  
+    def set_elset(self, start_line, element):
+        index = self.return_insert_position(start_line)
+        start = index + 1
+        
+    def add_to_elset(self, start_line, numbers):
+        index = self.return_insert_position(start_line)
+        start = index + 1
+        line = []
+        for num in numbers:
+            if len(line) == 16:
+                line_str = ''
+                for index, word in enumerate(line):
+                    if index == 15:
+                        line_str = line_str + ',' + str(word + 1) + '\n'
+                        line_str = line_str[1:]
+                        self.contents.insert(start, line_str)
+                        start += 1
+                    else:
+                        line_str = line_str + ',' + str(word + 1)
+                line = []     
+            line.append(num)
+        line_str = ''
+        for index, li in enumerate(line):
+            if index != len(line) - 1:
+                line_str = line_str + ',' + str(li + 1) 
+            else:
+              #  line_str = line_str[1:]
+                line_str = line_str + ',' + str(li + 1) + '\n'
+                line_str = line_str[1:]
+                self.contents.insert(start, line_str)
+                
+    # delete the element set, but maintain the start line of the set    
+    def delete_elset(self, start_line):
+        index = self.return_insert_position(start_line)
+        end = index + 1
+        line = self.contents[end].strip().split(',')
+        while line[0].isdigit():
+            self.contents.pop(end)
+            line = self.contents[end].strip().split(',')
+        
