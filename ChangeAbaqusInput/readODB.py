ba7e540a2baf5ccdd9b7e560692e7ec3e70d33cd
KuochengWang <30810210+KuochengWang@users.noreply.github.com>
2019-10-18 19:03:49 -0500

    fixed bugs
    the odb file does not output node displacement by order, this commit reorders the displacements

--- ChangeAbaqusInput/readODB.py
+++ ChangeAbaqusInput/readODB.py
@@ -6,22 +6,10 @@ import os
 import pdb
 import read_tetgen
 
-# odb file repeat displacements
-# @param odb the .odb file pointer
-# @param step_name the step name from Abaqus file
-# @param frame_num which frame data you want to extract
-# return an np array of displacement
-def read_frame(odb, step_name, frame_num):
-    if len(odb.steps[step_name].frames)== 0:
-      return
-    frame = odb.steps[step_name].frames[frame_num]
-    displacement = frame.fieldOutputs['U']    
-    field_values = displacement.values
-    disps = []
-    for v in field_values:
-        temp_disp = [v.data[0], v.data[1], v.data[2]]
-        disps.append(temp_disp)
-    return np.array(disps)
+# read displaements from all frames from odb files in a folder and write to text file  
+
+# Notice, everytime run, should delete the file already existed in the folder
+# Otherwise, it will append to the end of the same file
 
 # write displacements to txt files
 # arguments:
@@ -32,38 +20,57 @@ def write_all_frame(odb_files, step_name, num_breastnode, folder):
     for file in odb_files:
         odb = openOdb(file)
         total_frames = len(odb.steps[step_name].frames)
-        for frame in range(total_frames):
-            points = read_frame(odb, step_name, frame)
-            breast_points = points[0:num_breastnode]
-            f = open(folder + '\/' + file.split('.')[0] + '_' + str(frame) + '.txt', "a+")
-            rows = num_breastnode
-            for i in range(rows):
-                f.write(str(breast_points[i][0]) + ' ' + str(breast_points[i][1]) \
-                        + ' ' + str(breast_points[i][2]) + '\n')
-            f.close()
-# calculate coordinate after displacement
-# @param node_file .node from TetGen
-# @param element_file .ele from TetGen 
-# return the coordination after move 
-'''
-def move_breast(node_file, element_file, odb_file, step_name):
-    disps = read_lastframe(odb_file, step_name)
-    
-    content = read_tetgen.ReadTetGen(element_file, node_file)
-    original_coords = content.read_coordinates()
-    moved_coords = []
-    for i, coord in enumerate(original_coords):
-        moved_coords.append(coord + disps[i])
-    return moved_coords
-'''
+        for frame_num in range(total_frames):
+            positions = read_frame(odb, folder, step_name, frame_num)
+            output_file = open(folder + '\/' + file.split('.')[0] + '_' + \
+                               str(frame_num) + '.txt', "a+")
+            for pos in positions:
+                output_file.write(str(pos[0]) + ' ' + str(pos[1]) + ' ' + \
+                                  str(pos[2]) + '\n') 
+            output_file.close()
+        odb.close()
+
+# read postion from each specific frame
+# arguments:
+# odb: from openOdb(file)
+# step: the step name from Abaqus
+# folder: the filder to store file 
+# frame_num: which frame to read
+# returns:
+# a 2D list         
+def read_frame(odb, folder, step, frame_num):
+    disps_dic = dict() # a dictionary maps displacement to its indices
+    breast_part = 'PART-1-1'
+    for name, instance in odb.rootAssembly.instances.items(): 
+        if name == breast_part: 
+            numNodesTotal = len(instance.nodes) 
+            frame = odb.steps[ 'Step-1' ].frames[frame_num]   
+            displacement = frame.fieldOutputs['U']  
+            field_values = displacement.values            
+            index = 0            
+            for field_value in field_values:
+                label = field_value.nodeLabel
+                if len(field_value.instance.nodes) != len(instance.nodes):
+                    continue                            
+                x, y, z = field_value.data       
+                disps_dic[label] = [x, y, z] 
+                index += 1            
+    displacements = []
+    for index in range(numNodesTotal):
+        pos = disps_dic[index + 1]
+        displacements.append(pos)
+    return displacements        
+        
+
 # insert node positions to .inp
 # @param file_name the .inp need to be written into
 # @param positions the node positions
 def insert_nodes(file_name, positions, part):
     abaqus = manipulate_abaqus_file.ReadAbaqusInput(file_name)
     abaqus.add_node(positions, file_name, part)
-
+    
 if __name__ == "__main__":
+    
     odb = ['278.odb']
     step = 'Step-1'
     node = 'Skin_Layer.node'
@@ -76,18 +83,28 @@ if __name__ == "__main__":
     abaqus = manipulate_abaqus_file.ReadAbaqusInput(inp_file)
     breast_points = abaqus.read_part(part)
     num_breastnode = len(breast_points)
-    write_all_frame(odb, step, num_breastnode, output_folder)
-    points = read_frame(odb, step, frame_num)
-    breast_points = points[0:(num_breastnode - 1)]
-    hand_points = points[num_breastnode:]
-   # read_frame(odb, step, frame_num, part_instance, part)
-    
-    
-    pdb.set_trace()
-  #  positions = move_breast(node, element, odb, step)
-  #  insert_nodes(output_file, positions, part)
+    write_all_frame(odb, step, num_breastnode, output_folder)   
     
       
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
     
 
 
