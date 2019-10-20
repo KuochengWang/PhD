9eeebf154917c8edba06e30cde873b0856e16787
KuochengWang <30810210+KuochengWang@users.noreply.github.com>
2019-10-19 19:36:13 -0500

    improved write_to_file method

--- ChangeAbaqusInput/convert_tetgen_to_unity.py
+++ ChangeAbaqusInput/convert_tetgen_to_unity.py
@@ -5,13 +5,19 @@ Created on Tue Oct 15 21:41:01 2019
 @author: vr_lab
 """
 import read_tetgen
-from random_select_point_from_organ import write_to_file  
+
 
 # arg:
 # filename: the output file you want to write to
 # points: a list of 3D points
-def write_to_file(filename, points):
-    output_file = open(filename, 'w')
+# write_flag: whether to rewrite ('w') or append('a')
+def write_to_file(filename, points, write_flag):
+    if write_flag == 'write':
+        output_file = open(filename, 'w')
+    elif write_flag == 'append':
+        output_file = open(filename, 'a+')
+    else:
+        return
     for index, point in enumerate(points):
         output_file.write(str(point[0]) + ' ' + str(point[1]) + 
                           ' ' + str(point[2]) + '\n')
@@ -26,5 +32,5 @@ if __name__ == "__main__":
     tet_gen = read_tetgen.ReadTetGen(elefile, nodefile)
     triangles = tet_gen.read_surface(surfacefile)
     coords = tet_gen.read_coordinates()
-    write_to_file(triangles_unity, triangles)
-    write_to_file(nodefile_unity, coords)
+    write_to_file(triangles_unity, triangles, 'write')
+    write_to_file(nodefile_unity, coords, 'write')
