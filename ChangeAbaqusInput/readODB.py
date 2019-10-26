ed446441727c2086a37408a7eae9747fc8d22c40
KuochengWang <30810210+KuochengWang@users.noreply.github.com>
2019-10-26 11:51:55 -0500

    add remove move breast weight func

--- ChangeAbaqusInput/readODB.py
+++ ChangeAbaqusInput/readODB.py
@@ -5,13 +5,17 @@ import odbAccess
 from odbAccess import openOdb
 import os
 import pdb
+import read_tetgen
+import sys 
+
+# to run: abaqus python readODB.py unload
 
 class ReadOdb:
     # arguments:
     # odb_files: a list of odb file names
     def __init__(self, odb_files):
         self.odb_files = odb_files
-                    
+        
     # read displaements from all frames from odb files in a folder and write to text file  
     # Notice, everytime run, should delete the file already existed in the folder
     # Otherwise, it will append to the end of the same file
@@ -27,7 +31,7 @@ class ReadOdb:
             odb = openOdb(file)
             total_frames = len(odb.steps[step_name].frames)
             for frame_num in range(total_frames):
-                args = [odb, folder, step_name, part_name, frame_num]
+                args = [odb, step_name, part_name, frame_num]
                 positions = self.read_frame(args)
                 file_name = folder + '\/' + file.split('.')[0] + '_' + \
                                    str(frame_num) + '.txt'                
@@ -35,17 +39,17 @@ class ReadOdb:
                 file_names.append(file_name)
             odb.close()
         return file_names
-    # read postion from each specific frame
+    
+    # read all point position of a part from each specific frame
     # arguments:
     # odb: from openOdb(file)
     # step: the step name from Abaqus
     # part: the part name from Abaqus
-    # folder: the filder to store file 
     # frame_num: which frame to read
     # returns:
     # a 2D list         
     def read_frame(self, args):
-        odb, folder, step, part, frame_num = args
+        odb, step, part, frame_num = args
         disps_dic = dict() # a dictionary maps displacement to its indices
         for name, instance in odb.rootAssembly.instances.items(): 
             if name == part: 
@@ -67,7 +71,7 @@ class ReadOdb:
             displacements.append(pos)
         return displacements        
     
-    # read postion and displacement of a frame
+    # read a specific point postion and displacement of a frame
     # arguments:
     # odb: from openOdb(file)
     # step_name: the step name from Abaqus
@@ -85,12 +89,13 @@ class ReadOdb:
                 
                 for field_value in field_values:
                     label = field_value.nodeLabel    
-                    if len(field_value.instance.nodes) != len(instance.nodes):
+                    if len(field_value.instance.nodes) != len(instance.nodes) or label != node_label:
                         continue
+                    pdb.set_trace()
                     return field_value.instance.nodes[node_label - 1].coordinates, \
                            field_value.data
     
-    # returns the start and end position of a point
+    # returns the start and end position of a point of all frames 
     # returns:
     # a list of start positions, a list of end positions of all odb files
     def read_start_end_pos(self, step_name, node_label, part_name):
@@ -101,10 +106,13 @@ class ReadOdb:
             total_frames = len(odb.steps[step_name].frames)
             for frame_num in range(total_frames):
                 args = [odb, step_name, frame_num, node_label, part_name]
+                
                 pos, disp = self.readpoint_specific_frame(args)
+                
                 start_positions.append(pos) 
                 end_positions.append(pos + disp)
-        return start_positions, end_positions
+               
+        return start_positions, end_positions 
 
 # add the start and end position to the end of displacement file
 # args:
@@ -113,40 +121,68 @@ def write_hand_pos(filenames, start_positions, end_positions):
     for file, start, end in zip(filenames, start_positions, end_positions):
         write_to_file(file, [start], 'append')
         write_to_file(file, [end], 'append')
+    
+# calculate the center of a part
+# args:
+# inp_file, .inp file name
+# eleset, which element set does the part belong to 
+# odb_reader, ReadOdb class
+# step_name: step name in odb file
+# part: the part name in odb file
+# part_start: the starting line of the part in .inp file
+def find_centers(inp_file, eleset, odb_reader, step_name, part, part_start):
+    abaqus = manipulate_abaqus_file.ReadAbaqusInput(inp_file)
+    elements = abaqus.read_elset_or_nset(eleset)
+    element_to_node = abaqus.read_element(part_start)
+    node_indices = []
+    for elem in elements:
+        node_indices.append(element_to_node[elem - 1])  
+    nodes_unique = np.unique(np.array(node_indices))
+    positions_across_frame = []
+    
+    for label in nodes_unique:
+        start, end = odb_reader.read_start_end_pos(step_name, label, part)
+        positions_across_frame.append(np.array(start) - np.array(end))
+    pdb.set_trace()
 
 if __name__ == "__main__": 
-    odb_file = ['278.odb']
+    odb_files = ['278.odb']
     breast_step = 'Step-1'
     hand_step = 'Step-1'
-    tip_label = 4  # generate_all_scenario.py can find the label of hand tip
+    tip_label = 4  # generate_scenario.py can find the label of hand tip
     output_folder = 'displacement files'  
-    breast_part = 'PART-1-1'
     hand_part = 'PART-2-1'
-    odb_reader = ReadOdb(odb_file)
-    output_filenames = odb_reader.write_all_frame(breast_step, breast_part, output_folder)
-    pdb.set_trace()
-    hand_start, hand_end = odb_reader.read_start_end_pos(hand_step, tip_label, hand_part)
-    write_hand_pos(output_filenames, hand_start, hand_end)
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+    tumor_elset = '*Elset, elset=TUMOR_FROMSKIN\n'
+    inp_file = 'Palpation Jobs/surface_to_node.inp' 
+    odb_reader = ReadOdb(odb_files)
+    breast_part = 'PART-1-1'
+    breast_part_start = '*Part, name=PART-1\n'
+  #  find_centers(inp_file, tumor_elset, odb_reader, breast_step, breast_part, breast_part_start)
+  #  output_filenames = odb_reader.write_all_frame(breast_step, breast_part, output_folder)
+  #  for name in output_filenames:
+        
+  #  hand_start, hand_end = odb_reader.read_start_end_pos(hand_step, tip_label, hand_part)
+  #  write_hand_pos(output_filenames, hand_start, hand_end)
     
-
-
-
+    purpose = sys.argv[1]  # purpose can be unload (unload breat with weight)
+    if purpose == 'unload':
+        odb_files = ['Abaqus_outputs/reference_pos.odb']
+        inp_output_folder = 'Weight Jobs'
+        inp_file = 'F:/Research/Breast with weight/real_skin/reference_pos.inp'
+        for file in odb_files:
+            odb = openOdb(file)
+            frame_num = -1
+            odb_reader = ReadOdb(file)
+            args = [odb, breast_step, breast_part, frame_num]
+            disps = odb_reader.read_frame(args)  
+            file_name = file.split('/')[-1]
+            file_name = output_folder + '\/' + file_name.split('.')[0] + '.txt'                
+            write_to_file(file_name, disps, 'write')
+            abaqus = manipulate_abaqus_file.ReadAbaqusInput(inp_file)
+            points = abaqus.read_part(breast_part_start)
+            for index, disp in enumerate(disps):
+                new_pos = np.array(points[index]) + np.array([float(disp[0]),float(disp[1]),float(disp[2])])
+                abaqus.edit_part(breast_part_start, index, str(new_pos[0]) + ',' + str(new_pos[1]) + ',' + str(new_pos[2]) + '\n')
+                file_name = file.split('/')[-1]
+                file_name = inp_output_folder + '/' + file_name.split('.')[0] + '.inp'
+            abaqus.write_output(file_name)
