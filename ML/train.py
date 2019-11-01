ca891f3aba99dfece35895ff2af275979f2f8d97
KuochengWang <30810210+KuochengWang@users.noreply.github.com>
2019-11-01 09:50:59 -0500

    write to text file instead of sending to Unity

--- ML/train.py
+++ ML/train.py
@@ -1,5 +1,8 @@
-# To run, python train.py --train_data=. --pretrained_file=checkpoint/cp7.h5 --mode=connet_to_unity
+# To run, python train.py --train_data=. --pretrained_file=ck1.h5 --mode=connet_to_unity_weight
 # python train.py --train_data=disps --checkpoint=ck1.h5 --mode=train
+# python train.py --train_data=disps --pretrained_file=ck1.h5 --mode=train
+# when there is checkpoint already, don't need checkpoint. checkpoint is for 
+# creating a new checkpoint
 
 # cp4.h5, 40 hidden unit: 472us/sample - loss: 0.0097 - mean_absolute_error: 0.0396 - mean_squared_error: 0.0097 - val_loss: 0.0102 - val_mean_absolute_error: 0.0404 - val_mean_squared_error: 0.0102
 # cp5.h5, 60 hidden unit: 2668/2668 [==============================] - 2s 679us/sample - loss: 0.0068 - mean_absolute_error: 0.0330 - mean_squared_error: 0.0068 - val_loss: 0.0067 - val_mean_absolute_error: 0.0329 - val_mean_squared_error: 0.0067
@@ -17,6 +20,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras import layers
+import timeit
 import sys
 sys.path.insert(0, 'F:/Research/FEA simulation for NN/stl')
 import generate_scenario
@@ -83,25 +87,26 @@ def filter_out_data(data, threshold):
       res += str(idx) + ',' + str(temp[0]) + ',' + str(temp[1]) + ',' + str(temp[2])
     else:
       res += str(idx) + ',' + str(temp[0]) + ',' + str(temp[1]) + ',' + str(temp[2]) + ','
-  #pdb.set_trace()
   return res
 
-def conv_to_float(data):
+# conver the string to the data format in NN
+# args:
+# data: string like '1,2,3'
+# returns:
+# numpy array
+def conv_to_weightpredc(data):
   data_float = data.split(',')
   try:
-    len(data_float) == 6
+    len(data_float) == 3
   except:
-    print('input must have 6 numbers')
-  res = np.zeros((1, 2, 3))
+    print('input must have 3 numbers')
+  res = np.zeros((1, 1, 3))
   res[0,0,0] = data_float[0]
   res[0,0,1] = data_float[1]
   res[0,0,2] = data_float[2] 
-  res[0,1,0] = data_float[3]
-  res[0,1,1] = data_float[4]
-  res[0,1,2] = data_float[5]
   return res
-  
-def connect_to_unity(model):
+
+def connect_to_unity(model, flag):
   host, port = '127.0.0.1', 25001
   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   sock.bind((host, port))
@@ -110,16 +115,26 @@ def connect_to_unity(model):
   while True:
     c, addr = sock.accept()
     data = c.recv(1024).decode('utf-8')
-    nnInput = conv_to_float(data)
+    if(flag == 'weight'):
+        nnInput = conv_to_weightpredc(data)
     disp = model.predict(nnInput, batch_size=None, verbose=0)
+    threshold = 0
+    
+    disp_str = filter_out_data(disp, threshold)
+   
    # disp_str = conv_to_string(disp) 
-    disp_str = filter_out_data(disp, 1)
+    start = timeit.timeit()
+    output_file = open('disp_prediction.txt', 'w')
+    output_file.write(str(disp_str))
+    output_file.close()
+    end = timeit.timeit()
     ddd = disp_str.split(',')
-    dfd = '1,.1,.1,.1,2,.1,.1,1'
     print(len(ddd))
-    c.sendall(disp_str.encode('utf-8'))
-    c.close()
-
+  #  print(end - start)
+  #  c.sendall(disp_str.encode('utf-8'))
+   # c.sendall("OK".encode('utf-8'))
+    c.close()    
+    
 # return the displacement on the surface. the surface indices are in order from small to large
 def get_surface_disp(triangles, disp):
     res = []
@@ -144,11 +159,9 @@ def main(args):
   mode = args.mode
   model = build_model(point_num, col_num)
   if mode == 'train':
-    inp_files = glob.glob('F:/Research/FEA simulation for NN/stl/Abaqus_outputs/weight/*inp') 
+    inp_files = glob.glob('F:/Research/FEA simulation for NN/stl/Abaqus_outputs/weight/glandular_fat_10_90/*inp') 
     gravity = '** Name: GRAVITY-1   Type: Gravity\n'
     idx = 0
-    for tri in list(triangles):
-      continue
     for file in inp_files:
       name = os.path.basename(file)
       name = os.path.join(os.path.dirname(disp_filenames[0]), name.split('.')[0] + '.txt')
@@ -157,8 +170,7 @@ def main(args):
         _, direction = abaqus.read_gravity(gravity)            
         nn_input[idx, 0 : 1, :] = direction
         temp_pos = read(name)
-        disp = np.array(get_surface_disp(triangles, temp_pos))           
-    
+        disp = np.array(get_surface_disp(triangles, temp_pos))       
         nn_output[idx, :] = np.reshape(disp, point_num * col_num)
         idx += 1
     if args.pretrained_file != 'None':
@@ -168,17 +180,18 @@ def main(args):
       checkpoint = args.checkpoint
     checkpoint = ModelCheckpoint(checkpoint, monitor='loss', verbose=1, save_best_only=True)
     callbacks_list = [checkpoint]
-    model.fit(nn_input, nn_output, epochs=1200, shuffle=True, validation_split=0.2, callbacks=callbacks_list, verbose=1)
+    model.fit(nn_input, nn_output, epochs=10000, shuffle=True, validation_split=0.2, callbacks=callbacks_list, verbose=1)
  # model.evaluate()
   elif mode == 'infer':
     model.load_weights(args.pretrained_file)
     x = np.array([[[0, -20, 0], position[0]]])
     pdb.set_trace()
     res = model.predict(x, batch_size=None, verbose=0)
-  elif mode == 'connet_to_unity':
+  elif mode == 'connet_to_unity_weight':
    # pdb.set_trace()
     model.load_weights(args.pretrained_file)
-    connect_to_unity(model)
+    flag = 'weight'
+    connect_to_unity(model, flag)
     
 if __name__ == "__main__":
   arg_parser = argparse.ArgumentParser()
