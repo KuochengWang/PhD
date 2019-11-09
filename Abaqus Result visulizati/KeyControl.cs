70733ce7bd4386e536134cc0abe45300c66777e1
KuochengWang <30810210+KuochengWang@users.noreply.github.com>
2019-11-09 15:53:33 -0600

    rotate the breast

---
+++ Abaqus Result visulizati/KeyControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyControl : MonoBehaviour {
+
+    GameObject breast;
+
+	// Use this for initialization
+	void Start () {
+        breast = GameObject.Find("Breast");
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        receive breastScript = breast.GetComponent<receive>();
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+           breast.transform.RotateAround(breastScript.breastCenter, Vector3.forward, 2); ;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            breast.transform.RotateAround(breastScript.breastCenter, Vector3.back, 2); ;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            breast.transform.RotateAround(breastScript.breastCenter, Vector3.right, 2); ;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            breast.transform.RotateAround(breastScript.breastCenter, Vector3.left, 2); ;
+        }
+	}
+}
