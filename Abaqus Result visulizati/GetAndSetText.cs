eefac91724ee347c823869025cdc4408bfadbd6e
KuochengWang <30810210+KuochengWang@users.noreply.github.com>
2019-11-10 09:51:05 -0600

    add text UI for gravity angle

---
+++ Abaqus Result visulizati/GetAndSetText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetAndSetText : MonoBehaviour {
+
+    public InputField angle;
+
+    public void setget()
+    {
+        GameObject breast = GameObject.Find("Breast");
+        receive breastScript = breast.GetComponent<receive>();        
+        if(float.Parse(angle.text) == null)
+        {
+            return;
+        }
+        float breast_angle;
+        breast_angle = float.Parse(angle.text);
+        Vector2 direction = new Vector3(Mathf.Cos(breast_angle * Mathf.Deg2Rad), Mathf.Sin(breast_angle * Mathf.Deg2Rad), 0);
+        breastScript.Prediction(direction);
+    }
+}
