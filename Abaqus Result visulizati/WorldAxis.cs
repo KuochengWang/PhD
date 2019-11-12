d4a6f30e270271584db4369e770d076803fde783
KuochengWang <30810210+KuochengWang@users.noreply.github.com>
2019-11-12 15:58:40 -0600

    draws the axes

---
+++ Abaqus Result visulizati/WorldAxis.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldAxis : MonoBehaviour
+{
+    public float size = 1f;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(Vector3.right * size + gameObject.transform.position, Vector3.zero + gameObject.transform.position);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(Vector3.up * size + gameObject.transform.position, Vector3.zero + gameObject.transform.position);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(Vector3.forward * size + gameObject.transform.position, Vector3.zero + gameObject.transform.position);
+        Gizmos.color = Color.white;
+    }
+}
