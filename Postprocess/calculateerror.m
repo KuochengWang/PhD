c9e73bff84d777a4f6aefe087f1b7ca4b1bf7980
KuochengWang <30810210+KuochengWang@users.noreply.github.com>
2019-12-13 15:48:41 -0600

    add function calculate error for each distance bin

--- Postprocess/calculateerror.m
+++ Postprocess/calculateerror.m
@@ -1,7 +1,7 @@
 clc;
 clear all;
-abaqus_disp_file = 'F:\Research\FEA simulation for NN\stl\Abaqus_outputs\weight\test\88.txt';
-predicted_disp_file = 'F:\Research\FEA simulation for NN\stl\Abaqus_outputs\weight\test\disp_prediction88.txt';
+abaqus_disp_file = 'F:\Research\FEA simulation for NN\stl\Abaqus_outputs\weight\test\12.txt';
+predicted_disp_file = 'F:\Research\FEA simulation for NN\stl\Abaqus_outputs\weight\test\disp_prediction12.txt';
 index_file = 'F:\Research\FEA simulation for NN\train_patient_specific\disp_prediction.txt';
 triangle_file = 'F:\Research\Breast with weight\Weight in Unity V2\Breast\Skin_Layer_reference.face';
 indices = dlmread(index_file);
@@ -13,15 +13,41 @@ triangles = triangles';
 triangles = unique(triangles(:));
 abaqus_disp = abaqus_disp(triangles(indices), :);
 
+
 predicted_disp = dlmread(predicted_disp_file);
-error = sqrt(sum((abaqus_disp - predicted_disp).^2,2));
+error = sqrt(sum((abaqus_disp - predicted_disp).^2, 2));
 sum(error)/25374
 histogram(error)
-save('error88.txt', 'error', '-ascii');
+save('error12.txt', 'error', '-ascii');
 
+%% calculate error for each bin 
+abaqus_disp_mag = sqrt(sum((abaqus_disp).^2, 2));
+edges = [0 2.8 5.6 8.4 11.2 14];
+[~,disp_bin] = histc(abaqus_disp_mag,edges);
+errorrange_bin = [1 2 3 4 5];
+dispvalue_bin = {};
+errorvalue_bin = {};
+error_avg = [];
+error_std = [];
+disp_bin_length = length(disp_bin);
+
+for i = 1:length(errorrange_bin)
+    dispvalue_bin(i,:) = {find(disp_bin == errorrange_bin(i))};
+end
+
+for i = 1:length(errorrange_bin)
+    error_indices = dispvalue_bin{i};
+    errorvalue_bin(i,:) = {error(error_indices)};
+end
+
+for i = 1:length(errorrange_bin)
+    mean(errorvalue_bin{i});
+    error_avg = [error_avg mean(errorvalue_bin{i})];
+    error_std = [error_std (std(errorvalue_bin{i}))];
+end
 
 %% read orginal distance and save only the surface points
-abaqus_dist_file = 'F:\Research\FEA simulation for NN\stl\Abaqus_outputs\weight\test\88_dist.txt'
+abaqus_dist_file = 'F:\Research\FEA simulation for NN\stl\Abaqus_outputs\weight\test\12_dist.txt'
 abaqus_dist = dlmread(abaqus_dist_file);
 abaqus_dist = abaqus_dist(triangles(indices), :);
 save(abaqus_dist_file, 'abaqus_dist', '-ascii');
